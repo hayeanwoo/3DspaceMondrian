bf7c7c8bd08c4df90ec05e4d21217e11d1f99b17
juha <50194405+hayeanwoo@users.noreply.github.com>
2022-06-17 01:57:23 +0900

    경로 ./이걸로 수정해봄.
    경로를
    url('index.html') => url('./index.html')  
    url('/index.html') => url('./index.html')
    이런식으로  '.' <-점으로 현재 디렉토리인지, 현재 디렉토리의 상단 폴더인지 구분시켜 줘야된다.
     
    ./ => 현재 디렉토리
    ../ => 현재 디렉토리의 상위폴더
    출처: https://eunhee-programming.tistory.com/164 [코드짜는 문과녀:티스토리]

--- preloard.js
+++ preloard.js
@@ -1,10 +1,10 @@
 
 
 function preload() {
-  img1 = loadImage('artwork1.jpg');
-  img2 = loadImage('add_texture.jpg');
-  img3 = loadImage('artwork2.jpg');
-  myFont =  loadFont('add_tway_fly.ttf');
+  img1 = loadImage('./artwork1.jpg');
+  img2 = loadImage('./add_texture.jpg');
+  img3 = loadImage('./artwork2.jpg');
+  myFont =  loadFont('./add_tway_fly.ttf');
 }
 
 
